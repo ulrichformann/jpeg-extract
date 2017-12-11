5b968cd483abe4f9e2619801729efd97554eb5f1
kkk <spam-kevkol@web.de>
2017-12-11 10:30:10 +0100

    [BREAKING] added error to callback, fixed jpeg being returned without EOI because of off-by-1-error, optimization

--- jpeg-extract.js
+++ jpeg-extract.js
@@ -1,43 +1,45 @@
-var extract = function(url,callback) {
+var soi = new Buffer(2);
+soi.writeUInt16LE(0xd8ff, 0);
+
+var eoi = new Buffer(2);
+eoi.writeUInt16LE(0xd9ff, 0);
+
+var extract = function(url, callback) {
 	var request = require('request');
 
 	var chunks = [];
 	var img;
 
-	var soi = new Buffer(2);
-	soi.writeUInt16LE(0xd8ff, 0);
-
-	var eoi = new Buffer(2);
-	eoi.writeUInt16LE(0xd9ff, 0);
-
 	var req = request(url);
 
-	req.on('error',function (error) {
+	req.on('error', function(error) {
 		req.abort();
-		console.log(error,url);
+
+		callback(error);
 	});
 
-	req.on('response',function (response) {
-		if(response.statusCode !== 200){
+	req.on('response', function(response) {
+		if(response.statusCode !== 200) {
 			req.abort();
-			console.log(response.statusCode,url);
+			return callback(response.statusCode);
 		}
+
 		req.on('data',function (data) {
-			if(chunks.length === 0){
+			if(chunks.length === 0) {
 				chunks.push(data.slice(data.indexOf(soi), data.length));
-			}
-			else{
-				if(data.indexOf(eoi) != -1){
-					chunks.push(data.slice(0, data.indexOf(eoi)));
+			} else {
+				if(data.indexOf(eoi) != -1) {
+					chunks.push(data.slice(0, data.indexOf(eoi) + 1));
 					img = new Buffer.concat(chunks);
 
 					req.abort();
-					callback(img);
-				}else{
+					callback(undefined, img);
+				} else {
 					chunks.push(data);
 				}
 			}
 		});
 	});
-}
+};
+
 module.exports = extract;
